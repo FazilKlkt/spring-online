63c0361828b25b5050bbfd3f473e8a540ecd7ce9
fazilklkt <fazil.husen6@gmail.com>
2026-07-02 22:44:16 +0530

    Initial commit: Spring Boot API project

---
+++ src/main/java/com/example/rest_service/GreetingController.java
@@ -0,0 +1,40 @@
+package com.example.rest_service;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController              
+@RequestMapping("/api")
+public class GreetingController {
+
+    @GetMapping("/hello")    
+    public String sayHello() {
+        return "Hello, Spring Boot!\n";
+    }
+    @GetMapping("/wadu")
+    public Object returnObj(){
+        return Map.of(
+                "message","hello bro",
+                "status",true,
+                "data",Map.of(
+                    "id",1,
+                    "name","aman"
+                    )
+                );
+    }
+    
+    @GetMapping("/wad1")
+    public Object returnObjj(){
+        return Map.of(
+                "message","hello bro",
+                "status",true,
+                "data",Map.of(
+                    "id",1,
+                    "name","aman"
+                    )
+                );
+    }
+}
